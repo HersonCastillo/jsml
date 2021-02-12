3b485e20fa26836bbd12b5a07743731c69b99306
Herson Castillo <hersoncastillo12@hotmail.com>
2021-02-11 23:17:12 -0600

    Stable version, uff

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { Component } from './core/component';
+export { Page } from './core/page';
+export { Routing } from './core/routing';
+export { ChangeDetection } from './core/change-detection';
+export * from './interfaces';
---
+++ src/interfaces/index.ts
@@ -0,0 +1,5 @@
+export { ComponentArguments } from './component-arguments';
+export { JSElement, JSElementEvent } from './js-element';
+export { PageArguments } from './page-arguments';
+export { PageElement } from './page-element';
+export { IRoute } from './routes';
